10060fef51591a628b304084bf9d5ca86f729fda
J Isle <sjisantillan@gmail.com>
2017-05-29 12:09:41 +0800

    Added static files and templates

--- orgsite/admin.py
+++ orgsite/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Organization, Activity
+from .models import Organization, Activity, FAQs
 
 admin.site.register(Organization)
 admin.site.register(Activity)
+admin.site.register(FAQs)
--- orgsite/static/js/navbar.js
+++ orgsite/static/js/navbar.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	$(window).on("scroll",function(){
   	var wn = $(window).scrollTop();
     if(wn > 50){
-    	$(".navbar").css("background","rgba(255, 255, 255)");
+    	$(".navbar").css("background","rgba(239, 240, 235)");
     }
     else{
     	$(".navbar").css("background","rgba(0,0,0,0)");
